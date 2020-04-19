a9a7c508cf9ebf8467dff5a9619fc4dfe3d56af5
Cerridwen <ogriff17@gmail.com>
2020-04-19 13:14:19 -0400

    Concert-this is working!

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
 
 /* var spotify = new Spotify(keys.spotify); */
 var spotify = require('node-spotify-api');   
@@ -11,8 +13,12 @@ var spotify = new spotify(keys.spotify);
 function processCommands(inputCommand, commandParam){
     switch (inputCommand){
         case 'spotify-this-song':
-            spotifyThis(commandParam);
+            spotifyThis(commandParam)
             break;
+            case 'concert-this':
+                concertThis(commandParam)
+                break;
+                
             default:
                 console.log("Invalid Command." + inputCommand);
     }
@@ -22,7 +28,7 @@ function spotifyThis(song){
     if(song === undefined){
         song = defaultSong;
     }
-    spotify.search({type:'track', query:song}, function(err, data) {
+    spotify.search({type:'track', query:song}, function(err, data){
         if (err){
             console.log('Error ' + err);
             return;
@@ -40,7 +46,32 @@ function spotifyThis(song){
 
     )}
 
+    var  defaultArtist = "RKCB";
+   function concertThis(artist){
+       var queryUrl = "https://rest.bandsintown.com/artists/" + decodeURI(artist) + "/events?app_id=codingbootcamp";
+   
+        axios.get(queryUrl).then(
+        function(response)
+            {
+        console.log("Upcoming Events: " + artist);
+        console.log(response.data.length);
+        for (i=0; i< response.data.length; i++)
+        {
+            console.log(" Venue : " + response.data[i].venue.name);
+            console.log(" Country : " + response.data[i].venue.country);
+            console.log(" City : " + response.data[i].venue.city);
+            console.log(" Date : " + moment(response.data[i].datetime).format('MMMM Do YYYY, h:mm a'));
+            console.log("-----------------------");
 
-  /*   function bandsInTown () */
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+
+
+
+   }
+
 
 processCommands(inputCommand, commandParam);
